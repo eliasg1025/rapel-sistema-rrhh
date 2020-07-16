6bbf99587d52736742d10eb9440695e700fc0df3
Elias Guere <eliasguere1025@gmail.com>
2020-07-16 10:53:49 -0500

    #

--- app/Services/ReniecService.php
+++ app/Services/ReniecService.php
@@ -46,7 +46,7 @@ class ReniecService
         } else {
             if ($data->departamento == "CUSCO") {
                 $departamento_id = 8;
-            } else if ($data->departamento == "AMAZONAS") {
+            } elseif ($data->departamento == "AMAZONAS") {
                 $departamento_id = 1;
             } else {
                 $departamento_id = DB::table('departamentos')->where('name', $data->departamento)->first()->id;
