7b39f57a7b7ceed730414fde1117d09b91831dcc
Elias Guere <eliasguere1025@gmail.com>
2020-08-14 15:02:00 -0500

    Cambiando formato de las fechas

---
+++ app/Exports/FormulariosPermisoExport.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+
+class FormulariosPermisoExport implements FromArray, WithColumnFormatting
+{
+    use Exportable;
+
+    public $data;
+    public $cabeceras;
+
+    public function __construct(array $cabeceras, array $data)
+    {
+        $this->cabeceras = $cabeceras;
+        $this->data = $data;
+    }
+
+    /**
+     * @return array
+     */
+    public function array(): array
+    {
+        return [
+            $this->cabeceras,
+            $this->data
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'C' => '@',
+            'F' => '@',
+            'L' => '',
+            'M' => '',
+        ];
+    }
+}
