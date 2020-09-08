088786b4199aac92cabce95bad6b1b5a713d7d5e
Elias Guere <eliasguere1025@gmail.com>
2020-09-08 11:59:53 -0500

    Realizando la importacion

--- app/Http/Controllers/DocumentosTuReciboController.php
+++ app/Http/Controllers/DocumentosTuReciboController.php
@@ -76,7 +76,7 @@ class DocumentosTuReciboController extends Controller
                         'zona_labor_id' => ZonaLabor::getIdByTrabajador($nombre_archivo[0], $empresa_id),
                     ];
                 } catch (\Exception $e) {
-                    return response()->json($e->getMessage());
+                    return response()->json($e->getMessage() . ' -- ' . $e->getLine());
                     continue;
                 }
             }
