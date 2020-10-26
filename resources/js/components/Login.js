eaca808231226ee1152aed9ba131c7b68a2bb110
Elias Guere <eliasguere1025@gmail.com>
2020-10-26 14:08:14 -0500

    Estilizando la vista de login

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Layout, Card, Form, Input, Button, Spin, notification } from "antd";
+import { EyeInvisibleOutlined, EyeTwoTone  } from '@ant-design/icons';
 
 const Login = () => {
+
+    const [form, setForm] = useState({
+        username: '',
+        password: '',
+    });
+
+    const handleInput = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
         <Layout className="sign-in">
             <Layout.Content className="sign-in__content" style={{ height: "100vh" }}>
@@ -17,25 +31,42 @@ const Login = () => {
                     />
                 </h1>
 
-                <div className="card login">
+                <div className="card login" style={{ borderRadius: '4px' }}>
                     <div className="card-body">
                         <form method="POST" action="/login">
                             <div className="form-group">
-                                <label className="col-sm-3">Usuario</label>
+                                <label className="col-sm-3" style={{ fontSize: '13px' }}>Usuario</label>
                                 <div className="col-sm-12">
-                                    <input className="form-control" name="username" />
+                                    <Input
+                                        name="username"
+                                        placeholder="Ingrese nombre de usuario"
+                                        onChange={handleInput}
+                                    />
                                 </div>
                             </div>
                             <div className="form-group">
-                                <label className="col-sm-3">Contraseña</label>
+                                <label className="col-sm-3" style={{ fontSize: '13px' }}>Contraseña</label>
                                 <div className="col-sm-12">
-                                    <input type="password" className="form-control" name="password" />
+                                    <Input.Password
+                                        name="password"
+                                        placeholder="Ingrese contraseña"
+                                        iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                                        onChange={handleInput}
+                                    />
                                 </div>
                             </div>
+                            <br />
                             <div className="form-group">
-                                <button type="submit" className="btn btn-primary btn-block">
-                                    Ingresar
-                                </button>
+                                <div className="col-sm-12">
+                                    <button
+                                        type="submit"
+                                        className="btn btn-primary btn-lg btn-block"
+                                        style={{ backgroundColor: '#010066', borderColor: '#010066', borderRadius: '3px' }}
+                                        disabled={form.username === '' || form.password === '' || form.password.length < 6}
+                                    >
+                                        INGRESAR
+                                    </button>
+                                </div>
                             </div>
                         </form>
                     </div>
