b6ab83d8e71891b7a9fc87f0dcc1036a2dbe51b8
Elias Guere <eliasguere1025@gmail.com>
2020-11-14 12:27:17 -0500

    fix pusher implementation

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -31,7 +31,7 @@ window.Swal = require('sweetalert2');
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo';
+/* import Echo from 'laravel-echo';
 
 window.Pusher = require('pusher-js');
 
@@ -40,4 +40,4 @@ window.Echo = new Echo({
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     forceTLS: true
-});
+}); */
