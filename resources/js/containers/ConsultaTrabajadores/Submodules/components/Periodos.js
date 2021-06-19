2c65419c85877b2f4327b23f32d117881ac1fb4d
Elias Guere <eliasguere1025@gmail.com>
2021-06-19 06:41:58 -0500

    x

--- resources/js/containers/ConsultaTrabajadores/Submodules/components/Periodos.js
+++ resources/js/containers/ConsultaTrabajadores/Submodules/components/Periodos.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Table } from 'antd';
 
-const { usuario } = JSON.parse(sessionStorage.getItem('data'));
+const xd = JSON.parse(sessionStorage.getItem('data'));
 
 // console.log(usuario);
 
@@ -44,12 +44,12 @@ const columns = [
     },
 ];
 
-/* if (usuario?.modulo_rol?.tipo?.name !== 'SIN SUELDO') {
+if (xd?.usuario?.modulo_rol?.tipo?.name !== 'SIN SUELDO') {
     columns.push({
         title: 'Sueldo',
         dataIndex: 'sueldo'
     });
-} */
+}
 
 export const Periodos = ({ periodos }) => {
     return (
